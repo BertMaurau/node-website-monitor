d043e25aba6fc4cbdd39b85b7a7e2323e66faa5d
Bert Maurau <bert.maurau@live.be>
2017-10-02 09:21:44 +0200

    Added interval time via parameter

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
 var Monitor = require('./lib/monitor');
 
-const WEB_INTERVAL = 5; // Amount of seconds between each request
+
 const WEB_WEBSITES = [
-    'https://www.google.com'
+    'http://localhost'
 ]
 
 var activeMonitors = [];
 
+var interval_time = (process.argv[2]) ? process.argv[2] : 5; // Amount of seconds between each request
+
 WEB_WEBSITES.forEach(function(website) {
 
-    console.log('\x1b[34m%s\x1b[0m', '[' + Date.now() + '] ' + website + ' => Initializing a new Monitor..');
+    console.log('\x1b[34m%s\x1b[0m', '[' + Date.now() + '] ' + website + ' => Initializing a new Monitor.. (Interval: ' + interval_time + ')');
 
     // Init a new Monitor for each Website
-    activeMonitors.push(new Monitor({ url: website, interval: WEB_INTERVAL }));
+    activeMonitors.push(new Monitor({ url: website, interval: interval_time }));
 
 });
